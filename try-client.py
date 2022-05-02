d32180a48bf4f5440ef7c5ea3ae639395d2e7afc
kentypop <Levimcc7@hotmail.com>
2022-05-02 21:20:34 +0900

    FIRSTT commit

---
+++ try-client.py
@@ -0,0 +1,12 @@
+import requests
+
+endpoint = "http://127.0.0.1:8000/search/"
+
+get_response = requests.post(endpoint, json=
+    {
+   "user":1,
+   "query":"blood7"
+}
+    )
+
+print(get_response.json())
